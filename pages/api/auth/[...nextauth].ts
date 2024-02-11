095f9e85cc67725452f32b19b87cbab718c37868
bilaly <billycsk@gmail.com>
2024-02-11 12:46:17 +0100

    Connexion et déconexion réussie

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,16 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
+
 export const authOptions = {
-    // Configure one or more authentication providers
+    // secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
         // ...add more providers here
     ],
-}
+} satisfies NextAuthOptions
 
 export default NextAuth(authOptions)
