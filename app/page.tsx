e3cbc0c23d3ce4322b61fa3989464dd4dd87b065
bilaly <billycsk@gmail.com>
2024-02-13 09:54:19 +0100

    Clean HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-// import SignOutButton from "@/components/button/SignOutButton";
 import Presentation from "@/components/presentation/Presentation";
-// import { authOptions } from "@/pages/api/auth/[...nextauth]";
-// import { getServerSession } from "next-auth";
 
 export default async function Home() {
   return (
     <>
       <Presentation />
+      <hr />
     </>
   );
 }
